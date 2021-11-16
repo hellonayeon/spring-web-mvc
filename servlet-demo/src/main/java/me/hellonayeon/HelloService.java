3256cb2babdfc9a6c4fbb38a2f8e259d38122017
hellonayeon <hellonayeon@icloud.com>
2021-11-17 00:26:42 +0900

    스프링 IoC 컨테이너 연동

---
+++ servlet-demo/src/main/java/me/hellonayeon/HelloService.java
@@ -0,0 +1,10 @@
+package me.hellonayeon;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class HelloService {
+    public String getName() {
+        return "hellonayeon";
+    }
+}
--- servlet-demo/src/main/java/me/hellonayeon/HelloServlet.java
+++ servlet-demo/src/main/java/me/hellonayeon/HelloServlet.java
@@ -1,5 +1,8 @@
 package me.hellonayeon;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.web.context.WebApplicationContext;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -15,17 +18,18 @@ public class HelloServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         System.out.println("doGet");
+
+        ApplicationContext context = (ApplicationContext) getServletContext().getAttribute(WebApplicationContext.ROOT_WEB_APPLICATION_CONTEXT_ATTRIBUTE);
+        HelloService helloService = context.getBean(HelloService.class);
+        // HelloService 를 쓰고 있긴 하지만 직접적으로 new 해서 쓰는게 아니라 스프링이 제공해주는 IoC 컨테이너에 들어있는 Bean 을 사용
+
         resp.getWriter().println("<html>");
         resp.getWriter().println("<head>");
         resp.getWriter().println("<body>");
-        resp.getWriter().println("<h1>Hello," + getName() + "</h1>");
+        resp.getWriter().println("<h1>Hello," + helloService.getName() + "</h1>");
         resp.getWriter().println("</body>");
         resp.getWriter().println("</head>");
         resp.getWriter().println("</html>");
-    }
-
-    private Object getName() {
-        return getServletContext().getAttribute("name");
     }
 
     @Override
