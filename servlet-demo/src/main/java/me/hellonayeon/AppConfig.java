18d9aa16ea45f2c2127472e1f0e3a0a03e454f4e
hellonayeon <hellonayeon@icloud.com>
2021-11-17 01:11:29 +0900

    스프링 MVC 연동 (DispatcherServlet = Root WebApplicationContext - Servlet WebApplicationContext 계층구조)

--- servlet-demo/src/main/java/me/hellonayeon/AppConfig.java
+++ servlet-demo/src/main/java/me/hellonayeon/AppConfig.java
@@ -2,8 +2,11 @@ package me.hellonayeon;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Controller;
 
 @Configuration
-@ComponentScan
+// 기본으로는 모든 @Component 를 Bean 으로 등록
+// AppConfig = Root WebApplicationContext(Servlet WebApplicationContext's parent)
+@ComponentScan(excludeFilters = @ComponentScan.Filter(Controller.class))
 public class AppConfig {
 }
